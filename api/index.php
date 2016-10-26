f9d53b72c5fbb53f5840e8e3e3ce40a498e8f823
Renan GEHAN <renan@gehan.com>
2016-10-26 23:59:50 +0200

    Implementation API basique

--- api/index.php
+++ api/index.php
@@ -2,6 +2,9 @@
 
 require_once("./vendor/autoload.php");
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 //Crée l'api RESTful
 $app = new Silex\Application();
 $app['debug'] = true;
@@ -18,6 +21,22 @@ $app->get('/messages', function() use ($pdo) {
 	$data = $query->fetchAll(PDO::FETCH_ASSOC);
 
 	return json_encode($data);
+});
+
+$app->post('/messages', function(Request $request) use ($pdo) {
+	$message = $request->get('message');
+	$uid = $request->get('uid');
+
+	if($message == NULL || $uid == NULL)
+		return new Response('Bad parameters', 500);
+
+	$query = $pdo->prepare("INSERT INTO messages (id, uid, message) VALUES (NULL, :uid, :msg);");
+	$ret = $query->execute(['uid' => $uid, 'msg' => $message]);
+
+	if($ret)
+		return new Response('Message posted.', 201);
+	else
+		return new Response('Message couldn\'t be posted', 500);
 });
 
 $app->run();
