b56d6861212e4feef96f507bfb05758d74b8f2da
Renan GEHAN <renan@gehan.com>
2016-10-26 23:47:35 +0200

    Installation fonctionnelle

--- api/index.php
+++ api/index.php
@@ -2,8 +2,22 @@
 
 require_once("./vendor/autoload.php");
 
+//Crée l'api RESTful
+$app = new Silex\Application();
+$app['debug'] = true;
+
+//Le parser de fichier .env
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
+//Creation de la db
 $pdo = new PDO("mysql:host=" . getenv('DB_HOST') . ";dbname=" . getenv('DB_NAME'), getenv('DB_USER'), getenv('DB_PASS'));
 
+$app->get('/messages', function() use ($pdo) {
+	$query = $pdo->query("SELECT * FROM messages");
+	$data = $query->fetchAll(PDO::FETCH_ASSOC);
+
+	return json_encode($data);
+});
+
+$app->run();
