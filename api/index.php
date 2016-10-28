d30bd3fc10d303931ac1a0c8c09a10ab585cda4c
Renan GEHAN <renan@gehan.com>
2016-10-28 22:11:35 +0200

    Modification api

--- api/index.php
+++ api/index.php
@@ -17,6 +17,8 @@ $dotenv->load();
 $pdo = new PDO("mysql:host=" . getenv('DB_HOST') . ";dbname=" . getenv('DB_NAME'), getenv('DB_USER'), getenv('DB_PASS'));
 
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header("Access-Control-Allow-Methods: DELETE, GET, HEAD, POST, PUT, OPTIONS, TRACE");
 
 $app->get('/messages', function() use ($pdo) {
 	$query = $pdo->query("SELECT * FROM messages");
@@ -30,7 +32,7 @@ $app->post('/messages', function(Request $request) use ($pdo) {
 	$uid = $request->get('uid');
 
 	if($message == NULL || $uid == NULL)
-		return new Response('Bad parameters', 500);
+		return new Response('Bad parameters ('.$message.';'.$uid.')', 500);
 
 	$query = $pdo->prepare("INSERT INTO messages (id, uid, message) VALUES (NULL, :uid, :msg);");
 	$ret = $query->execute(['uid' => $uid, 'msg' => $message]);
@@ -40,5 +42,11 @@ $app->post('/messages', function(Request $request) use ($pdo) {
 	else
 		return new Response('Message couldn\'t be posted', 500);
 });
+
+$app->match("{url}", function($url) use ($app){
+	return "OK OPTIONS"; 
+})
+->assert('url', '.*')
+->method("OPTIONS");
 
 $app->run();
