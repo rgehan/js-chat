1a1cdddebc77d83dab02a0f4b798d9434ea9db8a
Renan GEHAN <renan@gehan.com>
2016-11-02 23:09:31 +0100

    Ajout d'un middleware d'auth, et d'une route d'ajout d'user /utilisateurs

--- api/index.php
+++ api/index.php
@@ -4,10 +4,7 @@ require_once("./vendor/autoload.php");
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-
-//Crée l'api RESTful
-$app = new Silex\Application();
-$app['debug'] = true;
+use Silex\Application;
 
 //Le parser de fichier .env
 $dotenv = new Dotenv\Dotenv(__DIR__);
@@ -20,6 +17,50 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header("Access-Control-Allow-Methods: DELETE, GET, HEAD, POST, PUT, OPTIONS, TRACE");
 
+/* 
+ * Crée l'api RESTful
+ */
+$app = new Application();
+$app['debug'] = true;
+
+//Middleware d'authentification
+$app->before(function(Request $request, Application $app) use ($pdo){
+	$user = $request->server->get('PHP_AUTH_USER');
+	$pw = $request->server->get('PHP_AUTH_PW');
+
+	//On recupere le salt correspondant à l'utilisateur
+	$saltRequest = $pdo->prepare("SELECT salt FROM users WHERE pseudo = :pseudo");
+	$saltRequest->execute(['pseudo' => $user]);
+	$data = $saltRequest->fetch(PDO::FETCH_ASSOC);
+
+	//Si on a pas de salt
+	if(!$data)
+	{
+		return new Response("Unauthorized", 401);
+	}
+	else
+	{
+		//On hash le password donné avec ce salt
+		$salt = $data['salt'];
+		$hash = hash('sha256', $salt . $pw);
+
+		//Et on le compare a celui existant en db
+		$passRequest = $pdo->prepare("SELECT COUNT(*) FROM users WHERE pseudo = :pseudo AND password = :hash");
+		$passRequest->execute(['pseudo' => $user, 'hash' => $hash]);
+	
+		$count = $passRequest->fetch()[0];
+
+		//Si on a pas d'utilisateur qui match, on ne poursuit pas...
+		if($count != 1)
+		{
+			return new Response("Unauthorized", 401);
+		}
+
+	}
+});
+
+
+//Recuperation des messages
 $app->get('/messages', function() use ($pdo) {
 	$query = $pdo->query("SELECT m.uid, m.message, u.pseudo FROM messages m INNER JOIN users u ON u.uid = m.uid ORDER BY m.id ASC");
 	$data = $query->fetchAll(PDO::FETCH_ASSOC);
@@ -27,6 +68,7 @@ $app->get('/messages', function() use ($pdo) {
 	return json_encode($data);
 });
 
+//Ajout d'un message
 $app->post('/messages', function(Request $request) use ($pdo) {
 	$message = $request->get('message');
 	$uid = $request->get('uid');
@@ -43,6 +85,28 @@ $app->post('/messages', function(Request $request) use ($pdo) {
 		return new Response('Message couldn\'t be posted', 500);
 });
 
+
+//Ajout d'un utilisateur
+$app->post('/utilisateurs', function(Request $request) use ($pdo) {
+	$user = $request->get('user');
+	$pw = $request->get('pw');
+
+	if($user == NULL || $pw == NULL)
+		return new Response('Bad parameters', 500);
+
+	$salt = uniqid(mt_rand(), true);
+	$hash = hash('sha256', $salt . $pw);
+
+	$query = $pdo->prepare("INSERT INTO users (uid, pseudo, password, salt) VALUES (NULL, :pseudo, :pass, :salt);");
+	$ret = $query->execute(['pseudo' => $user, 'pass' => $hash, 'salt' => $salt]);
+
+	if($ret)
+		return new Response('User added.', 201);
+	else
+		return new Response('User couldn\'t be added', 500);
+});
+
+//Censé authoriser les requetes OPTIONS
 $app->match("{url}", function($url) use ($app){
 	return "OK OPTIONS"; 
 })
