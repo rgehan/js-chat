23a3bba37454ede1543ffc087dc65746ed3fdf3b
Renan GEHAN <renan@gehan.com>
2016-11-08 00:15:44 +0100

    Added app base component

---
+++ client/src/components/App.vue
@@ -0,0 +1,24 @@
+<template lang="jade">
+    router-view
+</template>
+
+<script>
+    var app = {
+        name: 'App',
+        data: function(){
+            return {
+            }
+        },
+        components:{
+        
+        },
+        methods: {
+
+        }
+    };
+
+    export default app;
+</script>
+
+<style lang="stylus">
+</style>
